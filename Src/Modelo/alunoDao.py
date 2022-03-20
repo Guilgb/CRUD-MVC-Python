fdeb45341a689fd5d0fa9ebd437c81daefd40c0f
Guilherme Bezerra <guilherme.gb@hotmail.com>
2022-03-20 16:01:39 -0300

    ta quase

--- Src/Modelo/alunoDao.py
+++ Src/Modelo/alunoDao.py
@@ -11,82 +11,92 @@ class DAOAluno:
         pass
 
     def insertAluno(self):
-        nomePessoa = input('\nSeu Nome:')
-        telefone = input('\nSeu Telefone:')
-        uf = input('\nSeu Estado:')
-        cidade = input('\nSua Cidade:')
-        escolaridade = input('\nSua Escolaridade:')
+        try:
+            nomePessoa = input('\nSeu Nome:')
+            telefone = input('\nSeu Telefone:')
+            uf = input('\nSeu Estado:')
+            cidade = input('\nSua Cidade:')
+            escolaridade = input('\nSua Escolaridade:')
 
-        con = Conection.getConection('')
+            con = Conection.getConection('')
 
-        cursor = con.cursor()
-        cadastrarAluno = input("Deseja cadrastrar um Aluno (y/n): ")
-        if(cadastrarAluno == 'y'):
-            telefones = Telefones(telefone)
-            estados = Estado(uf)
-            cidades = Cidade(cidade, estados)
-            escolaridades = Escolaridade(escolaridade)
-            novoAluno = Aluno(nomePessoa, telefones, cidades, escolaridades)
+            cursor = con.cursor()
+            cadastrarAluno = input("Deseja cadrastrar um Aluno (y/n): ")
+            if(cadastrarAluno == 'y'):
+                telefones = Telefones(telefone)
+                estados = Estado(uf)
+                cidades = Cidade(cidade, estados)
+                escolaridades = Escolaridade(escolaridade)
+                novoAluno = Aluno(nomePessoa, telefones,
+                                  cidades, escolaridades)
 
-        # SQL TELEFONE
-        sqlTelefone = "INSERT INTO Telefones(numeroTelefone) value(%s)"
-        valueTelefone = (novoAluno.telefones.numeroTelefone)
-        cursor.execute(sqlTelefone, valueTelefone)
+            # SQL TELEFONE
+            sqlTelefone = "INSERT INTO Telefones(numeroTelefone) value(%s)"
+            valueTelefone = (novoAluno.telefones.numeroTelefone)
+            cursor.execute(sqlTelefone, valueTelefone)
 
-        def idTelefone():
-            sqlIdtelefone = "SELECT idTelefone FROM Telefones where numeroTelefone=%s"
-            valueIdTelefones = (novoAluno.telefones.numeroTelefone)
-            cursor.execute(sqlIdtelefone, valueIdTelefones)
-            resultTelefone = cursor.fetchone()
-            for reTelefone in resultTelefone:
-                return reTelefone
+            def idTelefone():
+                sqlIdtelefone = "SELECT idTelefone FROM Telefones where numeroTelefone=%s"
+                valueIdTelefones = (novoAluno.telefones.numeroTelefone)
+                cursor.execute(sqlIdtelefone, valueIdTelefones)
+                resultTelefone = cursor.fetchone()
+                for reTelefone in resultTelefone:
+                    return reTelefone
 
-        # SQL ESTADO
-        sqlEstado = "INSERT INTO Estado(nomeEstado) value(%s)"
-        valueEstado = (novoAluno.cidades.estado.estado)
-        cursor.execute(sqlEstado, valueEstado)
+            # SQL ESTADO
+            sqlEstado = "INSERT INTO Estado(nomeEstado) value(%s)"
+            valueEstado = (novoAluno.cidades.estado.estado)
+            cursor.execute(sqlEstado, valueEstado)
 
-        def idEstado():
-            sqlIdEstado = "SELECT idEstado FROM Estado where nomeEstado=%s"
-            valueIdEstado = (novoAluno.cidades.estado.estado)
-            cursor.execute(sqlIdEstado, valueIdEstado)
-            resultEstado = cursor.fetchone()
-            for reEstado in resultEstado:
-                return reEstado
+            def idEstado():
+                sqlIdEstado = "SELECT idEstado FROM Estado where nomeEstado=%s"
+                valueIdEstado = (novoAluno.cidades.estado.estado)
+                cursor.execute(sqlIdEstado, valueIdEstado)
+                resultEstado = cursor.fetchone()
+                for reEstado in resultEstado:
+                    return reEstado
 
-        # SQL CIDADE
-        sqlCidade = "INSERT INTO Cidades(nomeCidade) value(%s)"
-        valueCidade = (novoAluno.cidades.cidade)
-        cursor.execute(sqlCidade, valueCidade)
+            # SQL CIDADE
+            sqlCidade = "INSERT INTO Cidades(nomeCidade) value(%s)"
+            valueCidade = (novoAluno.cidades.cidade)
+            cursor.execute(sqlCidade, valueCidade)
 
-        def idCidade():
-            sqlIdCidade = "SELECT idCidade FROM Cidades where nomeCidade=%s"
-            valueIdCidade = (novoAluno.cidades.cidade)
-            cursor.execute(sqlIdCidade, valueIdCidade)
-            resultCidade = cursor.fetchone()
-            for reCidade in resultCidade:
-                return reCidade
+            def idCidade():
+                sqlIdCidade = "SELECT idCidade FROM Cidades where nomeCidade=%s"
+                valueIdCidade = (novoAluno.cidades.cidade)
+                cursor.execute(sqlIdCidade, valueIdCidade)
+                resultCidade = cursor.fetchone()
+                for reCidade in resultCidade:
+                    return reCidade
 
-        # SQL ESCOLARIDADE
-        sqlEscolaridade = "INSERT INTO Escolaridade(nomeEscolaridade) value(%s)"
-        valueEscolaridade = (novoAluno.escolaridades.escolaridade)
-        cursor.execute(sqlEscolaridade, valueEscolaridade)
+            # SQL ESCOLARIDADE
+            sqlEscolaridade = "INSERT INTO Escolaridade(nomeEscolaridade) value(%s)"
+            valueEscolaridade = (novoAluno.escolaridades.escolaridade)
+            cursor.execute(sqlEscolaridade, valueEscolaridade)
 
-        def idEscolaridade():
-            sqlIdEscolaridade = "SELECT idEscolaridade FROM Escolaridade where nomeEscolaridade=%s"
-            valueIdEscolaridade = (novoAluno.escolaridades.escolaridade)
-            cursor.execute(sqlIdEscolaridade, valueIdEscolaridade)
-            resultEscolaridade = cursor.fetchone()
+            def idEscolaridade():
+                sqlIdEscolaridade = "SELECT idEscolaridade FROM Escolaridade where nomeEscolaridade=%s"
+                valueIdEscolaridade = (novoAluno.escolaridades.escolaridade)
+                cursor.execute(sqlIdEscolaridade, valueIdEscolaridade)
+                resultEscolaridade = cursor.fetchone()
 
-            for reEscolaridade in resultEscolaridade:
-                return reEscolaridade
+                for reEscolaridade in resultEscolaridade:
+                    return reEscolaridade
 
-        # SQL Aluno
-        sqlAluno = "INSERT INTO Aluno(nomeAluno, idTelefone, idCidade, idEscolaridade) values(%s, %s, %s, %s)"
-        valuesAluno = (novoAluno.nomePessoa, idTelefone(),
-                       idCidade(), idEscolaridade())
-        cursor.execute(sqlAluno, valuesAluno)
-        con.commit()
+            # SQL Aluno
+            sqlAluno = "INSERT INTO Aluno(nomeAluno, idTelefone, idCidade, idEscolaridade) values(%s, %s, %s, %s)"
+            valuesAluno = (novoAluno.nomePessoa, idTelefone(),
+                           idCidade(), idEscolaridade())
+            cursor.execute(sqlAluno, valuesAluno)
+            con.commit()
+
+        except TypeError as error:
+            print("Failed ", error)
+
+        finally:
+            cursor.close()
+            con.close()
+            print('SQL Connection Close')
 
     def listarAlunos(self):
         try:
--- Src/Modelo/instrutorDAO.py
+++ Src/Modelo/instrutorDAO.py
@@ -10,70 +10,79 @@ class DAOInstrutor:
         pass
 
     def inserirInstrutor(self):
-        nomePessoa = input('\nSeu Nome:')
-        formacao = input("\nDigite sua formação: ")
-        telefone = input('\nSeu Telefone:')
-        uf = input('\nSeu Estado:')
-        cidade = input('\nSua Cidade:')
+        try:
+            nomePessoa = input('\nSeu Nome:')
+            formacao = input("\nDigite sua formação: ")
+            telefone = input('\nSeu Telefone:')
+            uf = input('\nSeu Estado:')
+            cidade = input('\nSua Cidade:')
 
-        con = Conection.getConection('')
+            con = Conection.getConection('')
 
-        cursor = con.cursor()
-        cadastrarInstrutor = input("Deseja cadrastrar um Aluno (y/n): ")
-        if(cadastrarInstrutor == 'y'):
-            telefones = Telefones(telefone)
-            estados = Estado(uf)
-            cidades = Cidade(cidade, estados)
-            novoInstrutor = Instrutor(
-                nomePessoa, telefones, cidades, formacao)
+            cursor = con.cursor()
+            cadastrarInstrutor = input("Deseja cadrastrar um Aluno (y/n): ")
+            if(cadastrarInstrutor == 'y'):
+                telefones = Telefones(telefone)
+                estados = Estado(uf)
+                cidades = Cidade(cidade, estados)
+                novoInstrutor = Instrutor(
+                    nomePessoa, telefones, cidades, formacao)
 
-        # SQL TELEFONE
-        sqlTelefone = "INSERT INTO Telefones(numeroTelefone) value(%s)"
-        valueTelefone = (novoInstrutor.telefones.numeroTelefone)
-        cursor.execute(sqlTelefone, valueTelefone)
+            # SQL TELEFONE
+            sqlTelefone = "INSERT INTO Telefones(numeroTelefone) value(%s)"
+            valueTelefone = (novoInstrutor.telefones.numeroTelefone)
+            cursor.execute(sqlTelefone, valueTelefone)
 
-        def idTelefone():
-            sqlIdtelefone = "SELECT idTelefone FROM Telefones where numeroTelefone=%s"
-            valueIdTelefones = (novoInstrutor.telefones.numeroTelefone)
-            cursor.execute(sqlIdtelefone, valueIdTelefones)
-            resultTelefone = cursor.fetchone()
-            for reTelefone in resultTelefone:
-                return reTelefone
+            def idTelefone():
+                sqlIdtelefone = "SELECT idTelefone FROM Telefones where numeroTelefone=%s"
+                valueIdTelefones = (novoInstrutor.telefones.numeroTelefone)
+                cursor.execute(sqlIdtelefone, valueIdTelefones)
+                resultTelefone = cursor.fetchone()
+                for reTelefone in resultTelefone:
+                    return reTelefone
 
-        # SQL ESTADO
-        sqlEstado = "INSERT INTO Estado(nomeEstado) value(%s)"
-        valueEstado = (novoInstrutor.cidades.estado.estado)
-        cursor.execute(sqlEstado, valueEstado)
+            # SQL ESTADO
+            sqlEstado = "INSERT INTO Estado(nomeEstado) value(%s)"
+            valueEstado = (novoInstrutor.cidades.estado.estado)
+            cursor.execute(sqlEstado, valueEstado)
 
-        def idEstado():
-            sqlIdEstado = "SELECT idEstado FROM Estado where nomeEstado=%s"
-            valueIdEstado = (novoInstrutor.cidades.estado.estado)
-            cursor.execute(sqlIdEstado, valueIdEstado)
-            resultEstado = cursor.fetchone()
-            for reEstado in resultEstado:
-                return reEstado
+            def idEstado():
+                sqlIdEstado = "SELECT idEstado FROM Estado where nomeEstado=%s"
+                valueIdEstado = (novoInstrutor.cidades.estado.estado)
+                cursor.execute(sqlIdEstado, valueIdEstado)
+                resultEstado = cursor.fetchone()
+                for reEstado in resultEstado:
+                    return reEstado
 
-        # SQL CIDADE
-        sqlCidade = "INSERT INTO Cidades(nomeCidade) value(%s)"
-        valueCidade = (novoInstrutor.cidades.cidade)
-        cursor.execute(sqlCidade, valueCidade)
+            # SQL CIDADE
+            sqlCidade = "INSERT INTO Cidades(nomeCidade) value(%s)"
+            valueCidade = (novoInstrutor.cidades.cidade)
+            cursor.execute(sqlCidade, valueCidade)
 
-        def idCidade():
-            sqlIdCidade = "SELECT idCidade FROM Cidades where nomeCidade=%s"
-            valueIdCidade = (novoInstrutor.cidades.cidade)
-            cursor.execute(sqlIdCidade, valueIdCidade)
-            resultCidade = cursor.fetchone()
-            for reCidade in resultCidade:
-                return reCidade
+            def idCidade():
+                sqlIdCidade = "SELECT idCidade FROM Cidades where nomeCidade=%s"
+                valueIdCidade = (novoInstrutor.cidades.cidade)
+                cursor.execute(sqlIdCidade, valueIdCidade)
+                resultCidade = cursor.fetchone()
+                for reCidade in resultCidade:
+                    return reCidade
 
-        # SQL INSTRUTOR
-        sqlInstrutor = "INSERT INTO Instrutor(nomeInstrutor, formacao, idTelefone, idCidade) values(%s, %s, %s, %s)"
-        valuesInstrutor = (novoInstrutor.nomePessoa, novoInstrutor.formacao,
-                           idTelefone(), idCidade()
-                           )
-        cursor.execute(sqlInstrutor, valuesInstrutor)
+            # SQL INSTRUTOR
+            sqlInstrutor = "INSERT INTO Instrutor(nomeInstrutor, formacao, idTelefone, idCidade) values(%s, %s, %s, %s)"
+            valuesInstrutor = (novoInstrutor.nomePessoa, novoInstrutor.formacao,
+                               idTelefone(), idCidade()
+                               )
+            cursor.execute(sqlInstrutor, valuesInstrutor)
 
-        con.commit()
+            con.commit()
+
+        except TypeError as error:
+            print("Failed", error)
+
+        finally:
+            cursor.close()
+            con.close()
+            print('CONNECTION CLOSE')
 
     def deleteInstrutor(self):
         try:
